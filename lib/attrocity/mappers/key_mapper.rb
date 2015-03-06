c40abd58db5824d62167dbb3d60c66f4f6bd7a6f
TJ Stankus <tjstankus@gmail.com>
2015-03-06 11:21:53 -0500

    [TS][89419066] Add unit spec for KeyMapper

---
+++ lib/attrocity/mappers/key_mapper.rb
@@ -0,0 +1,13 @@
+module Attrocity
+  class KeyMapper
+    attr_reader :key
+
+    def initialize(key)
+      @key = key
+    end
+
+    def call(_, attributes_data)
+      attributes_data[key]
+    end
+  end
+end
