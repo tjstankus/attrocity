237b6bfc5e633ee99a5dc71df858f3ce7d02ae08
TJ Stankus <tjstankus@gmail.com>
2015-03-11 18:34:50 -0400

    [TS][89419066] Prepare to remove Attribute#value

--- spec/attrocity/attribute_spec.rb
+++ spec/attrocity/attribute_spec.rb
@@ -3,22 +3,5 @@ require 'attrocity/coercers/integer'
 
 module Attrocity
   RSpec.describe Attribute do
-    describe '.initialize' do
-      it 'accepts parameters ...?'
-    end
-
-    describe '#deep_clone' do
-      it 'might work, this is a placeholder'
-    end
-
-    describe '#value=' do
-      it 'coerces value' do
-        name = key = :age
-        mapper = Attribute.default_mapper(key)
-        attr = Attribute.new(name, Coercers::Integer.new, mapper)
-        attr.value = '10'
-        expect(attr.value).to eq(10)
-      end
-    end
   end
 end
